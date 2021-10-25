dec8121691517879fedbbcc12b717f848eb97d67
temirKhan42 <52080219+temirKhan42@users.noreply.github.com>
2021-10-25 14:38:11 +0600

    Add react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,14 +1,53 @@
-import React, { Component } from "react";
 import "./main.scss";
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
-class App extends Component {
-  render(){
-    return(
-      <div className="App">
-        <h1> Hello, World! </h1>
+export default function App() {
+  return (
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route exact path="/login">
+            <Login />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+          <Route path="*">
+            <NoPage />
+          </Route>
+        </Switch>
       </div>
-    );
-  }
+    </Router>
+  );
 }
 
-export default App;
+function Home() {
+  return <h2>Home</h2>;
+}
+
+function Login() {
+  return <h2>Login</h2>;
+}
+
+function NoPage() {
+  return <h2>Error 404</h2>;
+}
