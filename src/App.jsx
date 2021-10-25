459f5c519e9d68736af27c13173dbed142cee15d
temirKhan42 <52080219+temirKhan42@users.noreply.github.com>
2021-10-25 15:59:32 +0600

    Auto Deploy heroku

--- src/App.jsx
+++ src/App.jsx
@@ -22,14 +22,12 @@ export default function App() {
           </ul>
         </nav>
 
-        {/* A <Switch> looks through its children <Route>s and
-            renders the first one that matches the current URL. */}
         <Switch>
-          <Route exact path="/login">
-            <Login />
-          </Route>
           <Route path="/">
             <Home />
+          </Route>
+          <Route path="/login">
+            <Login />
           </Route>
           <Route path="*">
             <NoPage />
