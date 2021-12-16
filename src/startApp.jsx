5d0a7abbb953b4feb516d2dd05b430f70fbc074d
temirKhan42 <52080219+temirKhan42@users.noreply.github.com>
2021-12-16 17:36:38 +0600

    Add rollbar-react lib

--- src/startApp.jsx
+++ src/startApp.jsx
@@ -4,12 +4,26 @@ import App from "./components/App.jsx";
 import store from "./store/index.js";
 import { Provider } from "react-redux";
 import "./i18n.js";
+import { Provider as RollbarProvider, ErrorBoundary } from '@rollbar/react';
+
+const rollbarConfig = {
+  accessToken: '893e040092a4437c8fd6aea77735d269',
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+  payload: {
+    environment: 'production',
+  },
+};
 
 const startApp = () => {
   ReactDOM.render(
-    <Provider store={store}>
-      <App />
-    </Provider>,
+    <RollbarProvider config={rollbarConfig}>
+      <ErrorBoundary>
+        <Provider store={store}>
+          <App />
+        </Provider>
+      </ErrorBoundary>
+    </RollbarProvider>,
     document.getElementById("chat")
   );
 };
