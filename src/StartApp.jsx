c41876ce9d796216ab0fcdad69125d33f81b0f99
temirkhan <52080219+temirKhan42@users.noreply.github.com>
2023-01-23 11:52:25 +0600

    fix test deploy2

--- src/StartApp.jsx
+++ src/StartApp.jsx
@@ -6,8 +6,8 @@ import store from './store/index.js';
 import './i18n.js';
 
 const StartApp = ({ socket }) => {
-  console.log('starting app');
-  
+  console.log('starting app 1');
+
   const rollbarConfig = {
     accessToken: 'cf96e5d807e5492fb9377e245ab7ebc3', // 'POST_CLIENT_ITEM_ACCESS_TOKEN'
     captureUncaught: true,
